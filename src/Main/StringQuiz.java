1dc7d9b3e6a6df2bd3ef368834a15236918fa5b1
lucas-alencar <lucasalencar.rd@gmail.com>
2022-06-23 17:27:00 -0300

    adding hasSpecialQuest method

--- src/Main/StringQuiz.java
+++ src/Main/StringQuiz.java
@@ -12,19 +12,21 @@ public class StringQuiz {
 	}
 	
 	public StringSpecifications startQuiz() throws Exception {
-		if(this.doAllQuestions()) {
-		return this.wishedString;
-		}
+		if(this.doAllQuestions()) 
+			return this.wishedString;
+		
 		return null;
 	}
 
 	private boolean doAllQuestions() throws Exception {
 		this.wishedString = new StringSpecifications();
-		this.question1();
+		this.questionMinStringLenght();
+		this.questionMaxStringLenght();
+		this.questionHasSpecialChars();
 		return true;
 	}
 	
-	private int question1() throws Exception {
+	private int questionMinStringLenght() throws Exception {
 		int minString = 0;
 		
 		System.out.println("enter the minimum string length");
@@ -34,5 +36,33 @@ public class StringQuiz {
 			}
 			this.wishedString.setMinString(minString);
 			return minString;
+	}
+	
+	private int questionMaxStringLenght() throws Exception {
+		int maxString = -1;
+		
+		System.out.println("enter the maximum string length");
+			maxString = scanner.nextInt();
+			if(maxString < 0) {
+				throw new Exception("Invalid minimum string length");
+			}
+			this.wishedString.setMaxString(maxString);
+			return maxString;
+	}
+	
+	private boolean questionHasSpecialChars() throws Exception{
+		System.out.println("do the password have special chars? (yes,no)");
+		String hasSpecialChars = scanner.nextLine();
+		if(hasSpecialChars == "yes") {
+			this.wishedString.setSpecialChars(true);
+			return true;
+		}
+		else if(hasSpecialChars == "no") {
+			this.wishedString.setSpecialChars(false);
+			return false;
+		}
+		else {
+			throw new Exception("invalid answer special chars: "+hasSpecialChars);
+		}
 	}
 }
